850db0c6714af86f976daa87b4f93e7542ea7e76
mhj-pc <5261387+markhj@users.noreply.github.com>
2024-03-03 15:58:16 +0100

    Godot material generator

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use std::env;
+use std::{env, io, thread};
 use std::fs;
+use std::fs::File;
+use std::io::{Read, Write};
 use std::path::{Path, PathBuf};
+use std::time::Duration;
 use image::{DynamicImage, ImageResult};
 use image::io::Reader as ImageReader;
 use regex;
 use regex::Regex;
 use clap::Parser;
+use rand::distributions::{Alphanumeric, DistString};
+use rand::thread_rng;
 
 #[derive(Parser, Debug)]
 #[command(name = "Godot Image to Material Converter")]
@@ -26,6 +31,42 @@ struct Options {
     /// No files will be converted when this flag is on
     #[arg(short, long, default_value_t = false)]
     preview: bool,
+
+    /// Generate a Godot StandardMaterial3D based on the converted files
+    /// This requires that the filenames contain hints such as "albedo" or "normal"
+    #[arg(short, long, default_value_t = false)]
+    material: bool,
+}
+
+/// Error types for the ``convert_file`` method.
+/// We want to handle errors differently, for instance when a file exists, it should
+/// not be excluded from the list which is passed onto the material generator
+enum ConversionError {
+    FailedToDecode,
+    FailedToConvert,
+    FileExists,
+}
+
+/// Godot Material Property
+/// Contains the supported material property types such as albedo, normal map and roughness
+enum GodotMaterialProperty {
+    AlbedoTexture,
+    NormalTexture,
+    HeightTexture,
+    RoughnessTexture,
+    MetallicTexture,
+    AmbientOcclusionTexture
+}
+
+/// Godot material mapping
+/// A result-type object which contains all information relevant to generate
+/// a Godot material such as source files, property type and UID
+struct GodotMaterialMapping {
+    path: PathBuf,
+    uid: String,
+    short_uid: String,
+    source_file: String,
+    property: GodotMaterialProperty,
 }
 
 fn main() {
@@ -60,20 +101,237 @@ fn process(options: Options) {
                   "Review the search pattern and make sure you're in the right directory.");
     }
 
+    // The list of converted (or existing conversion), which will be passed
+    // to the material generator
+    let mut converted_files: Vec<PathBuf> = Vec::new();
+
     // Iterate over each file and attempt to convert them
     for path in files {
+        // Store the original filename
+        let original = path.file_name().unwrap().to_str().unwrap();
+
         match convert_file(&path, &options) {
             Ok(new_path) => {
-                let original = path.file_name().unwrap().to_str().unwrap();
                 if options.preview {
                     println!("File {} would converted and moved to: {}", original, new_path.to_str().unwrap())
                 } else {
                     println!("OK: {}", original)
                 }
+                converted_files.push(new_path);
             },
-            Err(t) => eprintln!("{}", t),
+            Err(ConversionError::FailedToDecode) => eprintln!("Failed to decode: {}", original),
+            Err(ConversionError::FailedToConvert) => eprintln!("Failed to convert: {}", original),
+            Err(ConversionError::FileExists) => {
+                let new_path = generate_new_filename(&path, &options.destination);
+                println!("Exists: {}", new_path.file_name().unwrap().to_str().unwrap());
+                converted_files.push(new_path);
+            },
         }
     }
+
+    if options.material {
+        generate_material(&options, converted_files);
+    }
+}
+
+/// Generate a ``StandardMaterial3D`` based on the files that have been converted
+/// A requirement for this to work is that the files contain hints in their names
+/// such as "albedo" or "normal"
+///
+/// Currently supported hints:
+/// * albedo
+/// * normal
+/// * height
+/// * roughness
+/// * metallic
+/// * ao (Ambient Occlusion)
+fn generate_material(_options: &Options, files: Vec<PathBuf>) {
+    let mut files_found: Vec<PathBuf>;
+    let mut iters: i8 = 0;
+    let max_iters: i8 = 100;
+
+    loop {
+        files_found = Vec::new();
+
+        for f in &files {
+            let ext = f.extension().unwrap().to_str().unwrap();
+            let import_path = f.clone().with_extension(format!("{}.import", ext));
+            if import_path.exists() {
+                files_found.push(import_path);
+            }
+        }
+
+        if files_found.len() < files.len() {
+            if iters == 0 {
+                print!("{} {}",
+                       "Waiting for .import files.",
+                       "Make Godot window active. This will prompt it to create the .import files: .",
+                );
+            } else {
+                print!(".");
+            }
+
+            io::stdout().flush().unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+
+        iters += 1;
+        if iters >= max_iters || files_found.len() == files.len() {
+            break;
+        }
+    }
+
+    if files_found.len() < files.len() {
+        println!("Will not wait any longer for .import files");
+        return;
+    }
+
+    println!("\nGenerating material...");
+
+    let mut uid_mapping: Vec<GodotMaterialMapping> = Vec::new();
+
+    let uid_regex = Regex::new(r#"\buid="uid://([^"]+)""#).unwrap();
+    let sf_regex = Regex::new(r#"\bsource_file="(res://[^"]+)""#).unwrap();
+
+    for import_file in &files_found {
+        let mut file = File::open(import_file).expect("Failed to open import file");
+        let mut data = String::new();
+
+        let mut uid: Option<String> = None;
+        let mut source_file: Option<String> = None;
+
+        file.read_to_string(&mut data).expect("Failed to read lines from import file");
+
+        for line in data.lines() {
+            if let Some(captures) = uid_regex.captures(&line) {
+                if let Some(value) = captures.get(1).map(|m| m.as_str()) {
+                    uid = Some(value.to_owned());
+                }
+            }
+
+            if let Some(captures) = sf_regex.captures(&line) {
+                if let Some(value) = captures.get(1).map(|m| m.as_str()) {
+                    source_file = Some(value.to_owned());
+                }
+            }
+        }
+
+        let property: Option<GodotMaterialProperty> = get_godot_property(import_file);
+        if uid.is_some() && source_file.is_some() {
+            uid_mapping.push(GodotMaterialMapping {
+                path: import_file.clone(),
+                property: property.unwrap(),
+                uid: uid.unwrap(),
+                source_file: source_file.unwrap(),
+                short_uid: format!("{}_{}", uid_mapping.len() + 1, generate_godot_uid(5)),
+            });
+        }
+    }
+
+    if uid_mapping.len() != files_found.len() {
+        eprintln!("UID mapping does not match number of files.");
+        return;
+    }
+
+    let mut mat_data = String::new();
+
+    mat_data.push_str(
+        format!("[gd_resource type=\"StandardMaterial3D\" format=3 uid=\"uid://{}\"]\n\n",
+            generate_godot_uid(12)
+        ).as_str()
+    );
+
+    for res in &uid_mapping {
+        mat_data.push_str(format!(
+            "[ext_resource type=\"Texture2D\" path=\"{}\" uid=\"uid://{}\" id=\"{}\"]\n",
+            res.source_file,
+            res.uid,
+            res.short_uid
+        ).as_str());
+    }
+
+    mat_data.push_str("\n[resource]");
+
+    for prop in uid_mapping {
+        match prop.property {
+            GodotMaterialProperty::AlbedoTexture => {
+                mat_data.push_str(format!(
+                    "\nalbedo_texture = ExtResource(\"{}\")",
+                    prop.short_uid).as_str()
+                );
+            },
+            GodotMaterialProperty::NormalTexture => {
+                mat_data.push_str("\nnormal_enabled = true");
+                mat_data.push_str(format!(
+                    "\nnormal_texture = ExtResource(\"{}\")",
+                    prop.short_uid).as_str()
+                );
+            },
+            GodotMaterialProperty::RoughnessTexture => {
+                mat_data.push_str(format!(
+                    "\nroughness_texture = ExtResource(\"{}\")",
+                    prop.short_uid).as_str()
+                );
+            },
+            GodotMaterialProperty::HeightTexture => {
+                mat_data.push_str("\nheightmap_enabled = true");
+                mat_data.push_str(format!(
+                    "\nheightmap_texture = ExtResource(\"{}\")",
+                    prop.short_uid).as_str()
+                );
+            },
+            GodotMaterialProperty::MetallicTexture => {
+                mat_data.push_str("\nmetallic = 1.0");
+                mat_data.push_str(format!(
+                    "\nmetallic_texture = ExtResource(\"{}\")",
+                    prop.short_uid).as_str()
+                );
+            },
+            GodotMaterialProperty::AmbientOcclusionTexture => {
+                mat_data.push_str("\nao_enabled = true");
+                mat_data.push_str(format!(
+                    "\nao_texture = ExtResource(\"{}\")",
+                    prop.short_uid).as_str()
+                );
+            },
+        }
+    }
+
+    fs::write("material.tres", mat_data);
+}
+
+fn get_godot_property(path: &PathBuf) -> Option<GodotMaterialProperty> {
+    let filename = path.file_name().unwrap().to_str().unwrap();
+
+    if filename.contains("albedo") {
+        return Some(GodotMaterialProperty::AlbedoTexture);
+    }
+
+    if filename.contains("normal") {
+        return Some(GodotMaterialProperty::NormalTexture);
+    }
+
+    if filename.contains("height") {
+        return Some(GodotMaterialProperty::HeightTexture);
+    }
+
+    if filename.contains("roughness") {
+        return Some(GodotMaterialProperty::RoughnessTexture);
+    }
+
+    if filename.contains("metallic") {
+        return Some(GodotMaterialProperty::MetallicTexture);
+    }
+
+    if filename.contains("_ao") {
+        return Some(GodotMaterialProperty::AmbientOcclusionTexture);
+    }
+
+    None
+}
+
+fn generate_godot_uid(length: usize) -> String {
+    Alphanumeric.sample_string(&mut thread_rng(), length).to_lowercase()
 }
 
 /// If the user has requested a destination directory, we will first
@@ -109,7 +367,7 @@ fn create_destination_directory(options: &Options) -> Result<(), String> {
 /// Convert file
 /// The image is loaded into a ``DynamicImage`` instance, which can then be used
 /// to save the image as a new format
-fn convert_file(path: &PathBuf, options: &Options) -> Result<PathBuf, String> {
+fn convert_file(path: &PathBuf, options: &Options) -> Result<PathBuf, ConversionError> {
     let allow_overwrites = options.allow_overwrites;
     let destination = &options.destination;
 
@@ -118,7 +376,7 @@ fn convert_file(path: &PathBuf, options: &Options) -> Result<PathBuf, String> {
 
     // If reading the file failed, we'll abort
     if img.is_err() {
-        return Err(format!("Failed to decode: {}", path.file_name().unwrap().to_str().unwrap()));
+        return Err(ConversionError::FailedToDecode);
     }
 
     // Generate the new filepath
@@ -126,8 +384,7 @@ fn convert_file(path: &PathBuf, options: &Options) -> Result<PathBuf, String> {
 
     // If the path exists, and overwrites are not allowed, we abort
     if new_path.exists() && !allow_overwrites {
-        return Err(format!("File exists: {}",
-                           new_path.file_name().unwrap().to_str().unwrap()));
+        return Err(ConversionError::FileExists);
     }
 
     // If in preview mode, we will abort here to avoid carrying
@@ -142,7 +399,7 @@ fn convert_file(path: &PathBuf, options: &Options) -> Result<PathBuf, String> {
 
     // If saving failed, we abort
     if res.is_err() {
-        return Err(format!("Failed to convert: {}", path.file_name().unwrap().to_str().unwrap()));
+        return Err(ConversionError::FailedToConvert);
     }
 
     Ok(new_path.clone())
